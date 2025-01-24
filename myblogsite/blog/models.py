ab8ef56aed20ba506049807a93564a27d203d998
syfulsharif <sharif.syful@gmail.com>
2025-01-24 21:27:53 +0600

    Models for blog updated

---
+++ myblogsite/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import  timezone
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publsh = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.title
