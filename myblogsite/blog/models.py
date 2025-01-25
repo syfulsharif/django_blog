0300a9b308e18d8d080de80a58a7d8d265c219a5
syfulsharif <sharif.syful@gmail.com>
2025-01-25 14:38:17 +0600

    Models updated for database indexing

--- myblogsite/blog/models.py
+++ myblogsite/blog/models.py
@@ -6,6 +6,11 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    publsh = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-publish']
+        indexes= [models.Index(fields=['-publish']),]
     def __str__(self):
         return self.title
